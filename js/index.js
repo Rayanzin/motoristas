79ee0a683d71316b8aac4063a45e27494725f3f7
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-09-09 12:09:57 -0300

    atrelamento semi ok

--- js/index.js
+++ js/index.js
@@ -22,6 +22,10 @@ overlay.addEventListener('click', (e => {
     confirmar.classList.remove("active")
     edicao.classList.remove("active")
     addmotorista.classList.remove("active")
+    preencher.classList.remove("active")
+    errodecnh.classList.remove('active')
+    atrelamento.classList.remove("active")
+    paragrafo.innerHTML = ``
 }))
 function listarCarros(dados) {
     mark.innerHTML = `<option selected disabled value="selecione">Selecione</option>`
@@ -47,7 +51,7 @@ function listarDetalhes() {
     let car = dados.marca[mark.value].modelos[modelo.value]
     cor.innerHTML = ``
     mudarcor.innerHTML = ``
-    for(let i = 0; i <= car.cores.length-1;i++){
+    for (let i = 0; i <= car.cores.length - 1; i++) {
         cor.innerHTML += `<option>${car.cores[i].cor}</option>`
         mudarcor.innerHTML += `<option>${car.cores[i].cor}</option>`
     }
@@ -61,17 +65,18 @@ btn.addEventListener('click', (e => {
     e.preventDefault()
 }))
 let arrayplacas = []
+let arraycarros = []
 function inserirCarro() {
     if (arrayplacas.indexOf(placa.value) == -1) {
         if (mark.value != 'selecione' && modelo.value != 'selecione') {
             arrayplacas.push(placa.value)
             let car = dados.marca[mark.value]
             table.innerHTML += `
-                    <tr id="tr${arrayplacas.length - 1}">
-                        <td>${car.nome}</td>
-                        <td>${car.modelos[modelo.value].nome}</td>
-                        <td class="cor">${cor.value}</td>
-                        <td>${placa.value}</td>
+                    <tr>
+                        <td value="${car.nome}">${car.nome}</td>
+                        <td value="${car.modelos[modelo.value].nome}">${car.modelos[modelo.value].nome}</td>
+                        <td class="cor" value="${cor.value}">${cor.value}</td>
+                        <td value="${placa.value}">${placa.value}</td>
                         
                         <td>
                             <abbr title="Editar carro"><button onclick="editarCarro(this)"><box-icon type='solid' name='pencil'></box-icon></button></abbr>
@@ -82,6 +87,7 @@ function inserirCarro() {
          `
             addcarros.classList.remove("active")
             overlay.classList.remove("active")
+            arraycarros.push()
         }
     } else {
         erro.classList.add("active")
@@ -89,6 +95,9 @@ function inserirCarro() {
 }
 function fecharTelaDeErro() {
     erro.classList.remove('active')
+    errodecnh.classList.remove('active')
+    errodecnh.classList.remove('active')
+    preencher.classList.remove("active")
 }
 let lixeira
 function desejaExcluir(tr) {
@@ -107,31 +116,76 @@ function excluirCarro() {
     confirmar.classList.remove("active")
     overlay.classList.remove("active")
     arrayplacas.splice(placa.value, 1)
+    cnhs.splice(placa.value, 1)
+    
 }
 let corSelecionada
-function editarCarro(button){
+function editarCarro(button) {
     row = button.closest('tr')
     corSelecionada = row.querySelector('.cor')
     overlay.classList.add("active")
     edicao.classList.add("active")
 }
-function editarCor(){
+function editarCor() {
     corSelecionada.innerHTML = changeCor()
     overlay.classList.remove("active")
     edicao.classList.remove("active")
 }
-function changeCor(){
-  return mudarcor.value
+function changeCor() {
+    return mudarcor.value
 }
-function adicionarMotorista(){
+function adicionarMotorista() {
     addmotorista.classList.add("active")
     overlay.classList.add("active")
 }
-function tableCars(){
+function tableCars() {
     tabeladecarros.style.display = 'block'
     motorista.style.display = 'none'
 }
-function tableDrivers(){
+function tableDrivers() {
     tabeladecarros.style.display = 'none'
     motorista.style.display = 'block'
+}
+
+cnh.addEventListener('keyup', (e => {
+    let carteira = e.target.value.slice(0, 10)
+    e.target.value = carteira
+}))
+let cnhs = []
+function inserirMotorista() {
+    console.log(cnhs);
+    
+    if (cnhs.indexOf(cnh.value) == -1) {
+        if (driver.value != '' && cnh.value != '') {
+            cnhs.push(cnh.value)
+            motoristas.innerHTML += `
+                        <tr>
+                            <td class="nomedomotorista" value="${driver.value}">${driver.value}</td>
+                            <td class="estado">livre</td>
+                            <td>
+                                <abbr title="Detalhes"><button class="eye"><box-icon name='show-alt'></box-icon></button></abbr>
+                                <abbr title="Editar carro"><button onclick="atrelarCarro(this)"><box-icon type='solid' name='car'></box-icon></button></abbr>
+                                <abbr title="Excluir carro"><button class="lixo" value="${cnh.value}" onclick="desejaExcluir(this)" ><box-icon type='solid'
+                                            name='trash'></box-icon></button></abbr>
+                            </td>
+                        </tr>
+        `
+            addmotorista.classList.remove("active")
+            overlay.classList.remove("active")
+        } else {
+            preencher.classList.add("active")
+        }
+    }else{
+        errodecnh.classList.add("active")
+    }
+}
+let condutor
+function atrelarCarro(button){
+    row = button.closest('tr')
+    condutor = row.querySelector('.nomedomotorista')
+    paragrafo.innerHTML += `Selecione um carro para atrelar à ${condutor.innerText}`
+    overlay.classList.add("active")
+    atrelamento.classList.add("active")
+    carrosadicionados.innerHTML = `<option value="selecionar" selected disabled>Selecionar</option>`
+    carrosadicionados.innerHTML += `<option></option>`
 }
